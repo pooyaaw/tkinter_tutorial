1d3f37199db5cdb6fa6c607cdae47700942ad54e
Pouya <pooya667@gmail.com>
2025-12-08 02:24:16 +0330

    grids kann man auch so nutzen

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ from tkinter import *
 root = Tk()
 
 # these are label widgets
-myLabel1 = Label(root, text="Hello bruh!")
-myLabel2 = Label(root, text="I'm learing tkinter.")
+myLabel1 = Label(root, text="Hello bruh!").grid(row=0, column=0)
+myLabel2 = Label(root, text="I'm learing tkinter.").grid(row=1, column=5)
+myLabel3 = Label(root, text="        ").grid(row=1, column=1)
 
-myLabel3 = Label(root, text="        ")
-
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=5)
-myLabel3.grid(row=1, column=1)
+# myLabel1
+# myLabel2
+# myLabel3
 # so the rows and coluns are relative
 
 root.mainloop()
