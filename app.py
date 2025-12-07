b5b3e5c5937e729d9daa29d5a824733f7b4f1a4a
Pouya <pooya667@gmail.com>
2025-12-08 02:20:26 +0330

    using .grid

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from tkinter import *
 
+# this creates the window
 root = Tk()
 
-myLabel = Label(root, text="Hello bruh!")
+# these are label widgets
+myLabel1 = Label(root, text="Hello bruh!")
+myLabel2 = Label(root, text="I'm learing tkinter.")
 
-myLabel.pack()
+myLabel3 = Label(root, text="        ")
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=5)
+myLabel3.grid(row=1, column=1)
+# so the rows and coluns are relative
 
 root.mainloop()
