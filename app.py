a3034f89a4944ab3433c0d4e8fd2c69472348d8d
Pouya <pooya667@gmail.com>
2025-12-15 15:45:57 +0330

    added feauture that gets an entry, uses it for output of the button.

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ from tkinter import *
 # this creates the window
 root = Tk()
 
+# Entry Widget for entering data
+# fg and bg work on Entry too for changing foreground and background color
+e = Entry(root, width="50", borderwidth="5")
+myl = Label(root, text="first enter your name: ")
+myl.pack()
+e.pack()
+e.get()  # gets whatever you entered into the widget
+
 
 def myClick():
-    myLabel = Label(root, text="Look! I clicked a button!")
+    myLabel = Label(root, text="hello " + e.get())
     myLabel.pack()
 
 
-myButton = Button(root,  command=myClick, text="Click Here",
-                  padx=30, pady=30, fg="green", bg="black")
+myButton = Button(root,  command=myClick, text="then click Here",
+                  padx=30, pady=30, fg="brown", bg="lightgreen")
 # you cant write  command=myClick() because the () will have the myClick executed anyway.
 # and when you click it again it wont be executed every time you click it.
-
-
 myButton.pack()
 
 root.mainloop()
